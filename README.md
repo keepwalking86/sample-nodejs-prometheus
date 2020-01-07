d3e46768a99947fba78652d798a4ba593810946e
keepwalking <nguyenvodung@gmail.com>
2020-01-07 14:32:12 +0700

    Check response time

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Yêu cầu cài đặt : Node.js, npm, and pm2
 
-$ git clone git@github.com:keepwalking86/sample-nodejs-prometheus.git
+$ git clone https://github.com/keepwalking86/sample-nodejs-prometheus.git
 
 $ cd sample-nodejs-prometheus
 
@@ -11,3 +11,5 @@ $ npm install
 $ pm2 install index.js --name nodejs-prometheus
 
 Browser : http://localhost:3000/metrics
+
+Test response time: http://localhost:3000/home
--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 var express = require('express')
 var app = express()
-//Declare prometheus
 var promMid = require('express-prometheus-middleware');
+var fs = require('fs');
 app.use(promMid({
-  metricsPath: '/metrics',
-  collectDefaultMetrics: true,
-  requestDurationBuckets: [0.1, 0.5, 1, 1.5]
+    metricsPath: '/metrics',
+    collectDefaultMetrics: true,
+    requestDurationBuckets: [0.1, 0.5, 1, 1.5]
 }))
+app.get('/home', (req, res) => {
+    fs.readFile('index.html', function(err, data) {
+    if (!err) {
+        res.writeHead(200, {
+        'Content-Type': 'text/html'
+        });
+        res.write(data);
+        res.end();
+    } else {
+        console.log('error');
+    }
+    });
+});
 var port = process.env.PORT || 3000
 var server = app.listen(port, () => {
   console.log(`App listening on port ${port}!`)
